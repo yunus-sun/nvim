0f43e8f63a420793ae92b996a554ebbcd5722f9e
yunus <yusun@smail.nju.edu.cn>
2024-09-05 23:59:19 +0800

    add tH, tL, gt1-9 keybindings for bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -24,7 +24,20 @@ return {
         -- NOTE: buffer related remaps are no need, because :bd :bp, :bn are quite easy!
         { "<leader>tn", "<cmd>tabnew<CR>", { noremap = true, desc = "Open a new tab" } },
         { "<leader>tc", "<cmd>tabclose<CR>", { noremap = true, desc = "Close a new tab" } },
+
         { "<leader>th", "<cmd>tabprevious<CR>", { noremap = true, desc = "Go to the previous tab" } },
         { "<leader>tl", "<cmd>tabnext<CR>", { noremap = true, desc = "Go to the next tab" } },
+        { "<leader>tH", "<cmd>BufferLineGoToBuffer 1<CR>", { noremap = true, desc = "Go to the buffer 1" } },
+        { "<leader>tL", "<cmd>BufferLineGoToBuffer -1<CR>", { noremap = true, desc = "Go to the buffer -1" } },
+
+        { "<leader>tg1", "<cmd>BufferLineGoToBuffer 1<CR>", { noremap = true, desc = "Go to the buffer 1" } },
+        { "<leader>tg2", "<cmd>BufferLineGoToBuffer 2<CR>", { noremap = true, desc = "Go to the buffer 2" } },
+        { "<leader>tg3", "<cmd>BufferLineGoToBuffer 3<CR>", { noremap = true, desc = "Go to the buffer 3" } },
+        { "<leader>tg4", "<cmd>BufferLineGoToBuffer 4<CR>", { noremap = true, desc = "Go to the buffer 4" } },
+        { "<leader>tg5", "<cmd>BufferLineGoToBuffer 5<CR>", { noremap = true, desc = "Go to the buffer 5" } },
+        { "<leader>tg6", "<cmd>BufferLineGoToBuffer 6<CR>", { noremap = true, desc = "Go to the buffer 6" } },
+        { "<leader>tg7", "<cmd>BufferLineGoToBuffer 7<CR>", { noremap = true, desc = "Go to the buffer 7" } },
+        { "<leader>tg8", "<cmd>BufferLineGoToBuffer 8<CR>", { noremap = true, desc = "Go to the buffer 8" } },
+        { "<leader>tg9", "<cmd>BufferLineGoToBuffer 9<CR>", { noremap = true, desc = "Go to the buffer 9" } },
     }
 }
