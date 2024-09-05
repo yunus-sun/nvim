e490e143c0f564cd112350f579ff0fb85d436a70
yunus <yusun@smail.nju.edu.cn>
2024-09-06 00:04:28 +0800

    new build way

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,7 +22,14 @@ return {
         { "nvim-telescope/telescope-project.nvim" },
         {
             "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make",
+            build = function(plugin)
+                local obj = vim.system({'cmake', '-S.', '-Bbuild', '-DCMAKE_BUILD_TYPE=Release'}, {cwd = plugin.dir}):wait()
+                if obj.code ~= 0 then error(obj.stderr) end
+                obj = vim.system({'cmake', '--build', 'build', '--config', 'Release'}, {cwd = plugin.dir}):wait()
+                if obj.code ~= 0 then error(obj.stderr) end
+                obj = vim.system({'cmake', '--install', 'build', '--prefix', 'build'}, {cwd = plugin.dir}):wait()
+                if obj.code ~= 0 then error(obj.stderr) end
+            end
         },
         { "nvim-telescope/telescope-symbols.nvim" },
         { "GustavoKatel/telescope-asynctasks.nvim" },
