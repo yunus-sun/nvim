73427b49f679b0d4280e6ccae9d57ae742f313f8
yunus <yusun@smail.nju.edu.cn>
2024-09-20 22:07:09 +0800

    enable remote markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -5,6 +5,10 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = function() vim.fn["mkdp#util#install"]() end,
     config = function()
-        vim.g.mkdp_filetypes = { "markdown" }
+        vim.g.mkdp_open_to_the_world = 1
+        vim.g.mkdp_open_ip = '127.0.0.1'
+        vim.g.mkdp_port = 8889
+        vim.g.mkdp_browser = 'none'
+        vim.g.mkdp_echo_preview_url = 1
     end,
 }
